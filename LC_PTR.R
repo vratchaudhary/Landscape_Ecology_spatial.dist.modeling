942d228e47d6e4654eacd1150e1e8294b1d80cd2
Your Name <your@email.com>
2017-11-07 11:55:00 -0500

    Added plots for inland water data in the code

--- LC_PTR.R
+++ LC_PTR.R
@@ -1,12 +1,15 @@
 #Code for clipping the India Land Cover map for the PTR
 
 #---- Set working directory
-setwd("/Users/martaprat/Documents//LandscapeEcology/Landscape_Ecology")
-setwd("C://Users//mprat//Documents//LandEco-India//Landscape_Ecology")
+#setwd("/Users/martaprat/Documents//LandscapeEcology/Landscape_Ecology")
+#setwd("C://Users//mprat//Documents//LandEco-India//Landscape_Ecology")
 
 #---- libraries
+install.packages("sp")
+library(sp)
 library(raster)
 library(rgdal)
+
 
 
 #opern raster layer from all India
@@ -17,10 +20,22 @@ projection(ras) #check projection
 extent.new=extent(92.55,93.15,26.9,27.3)
 ras2 <- crop(ras,extent.new)
 
+##inland water
+#PLOT WATER shapefiles polygon
+PTR.INW.P<- readOGR("./IND_wat/IND_water_Areas_dcw.shp")
+project(PTR.INW)
+plot(PTR.INW)
+#PLOT WATER LINES
+PTR.INW.l<- readOGR("./IND_wat/IND_water_lines_dcw.shp")
+project(PTR.INW.l)
+plot(PTR.INW.l)
+
 #open shapefile for PTR
 PTR.limit <- readOGR("./Shapefiles/2013-14/Pakke Tiger Reserve Boundary.shp")
 projection(PTR.limit) #check projection is the same
+plot(PTR.limit)
 plot(PTR.limit, col = "black", add = TRUE)
+
 
 #crop
 PTR.LC <- mask(ras2,PTR.limit)
